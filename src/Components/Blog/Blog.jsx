391632c40325643b77b467db5dd19a44882dc9af
Abid0624 <abidkhan09989@gmail.com>
2024-11-08 02:45:00 +0600

    added to the bookmark part has been done

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
+import { FaBookmark } from "react-icons/fa";
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
   const {
     title,
     cover,
@@ -11,26 +12,32 @@ const Blog = ({ blog }) => {
     hashtags,
   } = blog;
   return (
-    <div>
+    <div className="mb-20">
       <img
-        className="w-full h-[400px]"
+        className="w-full h-[400px] mb-8"
         src={cover}
         alt={`cover picture of the title ${title}`}
       />
       <div className="flex justify-between">
-        <div className="flex gap-6">
+        <div className="flex gap-6 mb-4">
           <img className="w-14 h-14 rounded-full" src={author_img} alt="" />
           <div>
             <h3 className="text-2xl">{author}</h3>
             <p>{posted_date}</p>
           </div>
         </div>
-        <div>
+        <div className="flex justify-center items-center">
           <span>{reading_time} min read</span>
+          <button
+            onClick={() => handleAddToBookmark(blog)}
+            className="ml-2 text-slate-600 text-2xl"
+          >
+            <FaBookmark></FaBookmark>
+          </button>
         </div>
       </div>
       <h2 className="text-4xl">{title}</h2>
-      <p>
+      <p className="mt-4">
         {hashtags.map((hash, idx) => (
           <span key={idx}>
             <a href="">{hash}</a>
@@ -43,6 +50,7 @@ const Blog = ({ blog }) => {
 
 Blog.propTypes = {
   blog: PropTypes.object.isRequired,
+  handleAddToBookmark: PropTypes.func.isRequired,
 };
 
 export default Blog;
